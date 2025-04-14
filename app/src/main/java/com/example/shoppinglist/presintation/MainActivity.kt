d092ed4a8fdac50d1b4ee10a81f6fffd8a66bb40
i11usion <i11usiongid@gmail.com>
2025-04-14 19:00:16 +0300

    первая реализация presentation слоя

--- app/src/main/java/com/example/shoppinglist/presintation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presintation/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.shoppinglist.presintation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivityTest", it.toString())
         }
+        viewModel.getShopList()
+        viewModel.deleteShopItem(2)
+        viewModel.editShopItem(ShopItem("Name 52", 10, true, 4))
+        viewModel.getShopList()
     }
 }
