056b53c6a3ae2cf5445cea663eda0be1cac456c8
i11usion <i11usiongid@gmail.com>
2025-04-20 19:20:47 +0300

    реализация перехода на второй экран

--- app/src/main/java/com/example/shoppinglist/presintation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presintation/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +21,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             rvAdapter.submitList(it)
+        }
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
 
@@ -67,6 +73,8 @@ class MainActivity : AppCompatActivity() {
     private fun setUpClickListener() {
         rvAdapter.onShopItemClickListener = {
             Log.d("MainActivity", "${it.id} - ${it.name} - ${it.count} - ${it.enabled}")
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
